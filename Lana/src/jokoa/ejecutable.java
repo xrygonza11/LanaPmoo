13d3b32258f6068605eb1f71549270ac6651af6c
Usuario <Usuario@AITOR>
2020-06-01 13:58:59 +0200

    Ejecutable

--- Lana/src/jokoa/ejecutable.java
+++ Lana/src/jokoa/ejecutable.java
@@ -4,7 +4,7 @@ public class ejecutable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			ListaJokalariak.getNireListaJokalariak().partidaJolastu();
+			ListaJokalariak.getNireListaJokalariak().partidaHasieratu();
 	}
 
 }
